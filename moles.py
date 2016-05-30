0ea22f12847024eaeab81bcac7c69fd6b9ab1818
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-05-30 12:54:16 -0700

    More comprehensive skeleton

--- moles.py
+++ moles.py
@@ -3,10 +3,29 @@ import pygame
 pygame.init()
 
 SIZE = (600, 600)
+BLACK       = (0, 0, 0)
+WHITE       = (255, 255, 255)
+RED         = (200, 0, 0)
+GREEN       = (0, 200, 0)
+BLUE        = (0, 0, 200)
 
 scrn = pygame.display.set_mode(SIZE)
 running = True
+clock = pygame.time.Clock()
+FPS = 60
 
 while running:
+    clock.tick(FPS)
+
     for e in pygame.event.get():
         if e.type == pygame.QUIT: running = False
+
+        # Handle keyboard inputs
+
+    # Update the state
+
+    scrn.fill(BLACK)
+
+    # Draw the state
+
+    pygame.display.flip()
